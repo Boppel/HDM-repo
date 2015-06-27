225dd6c6bd9178319dfe993fb26fa27c88730c9e
Felix Beifuss <boppel@gmail.com>
2015-06-27 17:42:14 +0200

    MAD: iOS quatsch

---
+++ MAD_iOS_myFirstApp/MAD_iOS_myFirstApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MAD_iOS_myFirstApp
+//
+//  Created by Felix Beifuss on 18/05/15.
+//  Copyright (c) 2015 Felix Beifuss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
