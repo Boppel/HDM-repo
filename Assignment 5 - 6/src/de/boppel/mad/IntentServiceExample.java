38d825f34d07e9bc63c4cdc37b1554a3748bdf9f
Felix Beifuß <boppel@gmail.com>
2015-07-01 19:14:01 +0200

    MAD: Assignment 6

--- Assignment 5 - 6/src/de/boppel/mad/IntentServiceExample.java
+++ Assignment 5 - 6/src/de/boppel/mad/IntentServiceExample.java
@@ -1,19 +1,26 @@
 package de.boppel.mad;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class IntentServiceExample extends Activity {
 
 	private EditText text_inputTime;
+	private TextView text_desc;
 	private Button btn_start;
 	private int waitTime = 0;
 
@@ -23,7 +30,12 @@ public class IntentServiceExample extends Activity {
         setContentView(R.layout.activity_intent_service_example);
         
         text_inputTime = (EditText) findViewById(R.id.inputTime);
+        text_desc = (TextView) findViewById(R.id.desc);
         btn_start = (Button) findViewById(R.id.btn_wait);
+        
+        ServiceBroadCastReceiver receiver = new ServiceBroadCastReceiver();
+        IntentFilter filter = new IntentFilter("MY_INTENT_SERVICE_ACTION");
+        LocalBroadcastManager.getInstance(this).registerReceiver(receiver, filter);
         
         btn_start.setOnClickListener(new OnClickListener() {
 			
@@ -36,6 +48,15 @@ public class IntentServiceExample extends Activity {
 				getApplicationContext().startService(i);
 			}
 		});
+    }
+    
+    private class ServiceBroadCastReceiver extends BroadcastReceiver {
+    	@Override
+    	public void onReceive(Context context, Intent intent) {
+    		// TODO Auto-generated method stub
+    		int seconds = (int)intent.getExtras().getInt("result");
+    		text_desc.setText("du hast " + seconds + "s gewartet! wow.");
+    	}
     }
 
     @Override
--- Assignment 5 - 6/src/de/boppel/mad/WaitService.java
+++ Assignment 5 - 6/src/de/boppel/mad/WaitService.java
@@ -2,6 +2,7 @@ package de.boppel.mad;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 public class WaitService extends IntentService{
@@ -18,6 +19,10 @@ public class WaitService extends IntentService{
 			try {
 				wait(waitTime * 1000);
 				Log.e("wait time:", waitTime + " seconds");
+				// Assignment 6: send (local) broadcast to activity
+				Intent i = new Intent("MY_INTENT_SERVICE_ACTION");
+				i.putExtra("result", waitTime);
+				LocalBroadcastManager.getInstance(this).sendBroadcast(i);
 			} catch (Exception e) {
 				// TODO: handle exception
 				e.printStackTrace();
