77b094cbc37d48f3f5a32ee831caaa57f9cd7781
Felix Beifuß <boppel@gmail.com>
2015-08-22 12:29:37 +0200

    ?

--- Mobile App Development/Assignment 5 - 6/src/de/boppel/mad/IntentServiceExample.java
+++ Mobile App Development/Assignment 5 - 6/src/de/boppel/mad/IntentServiceExample.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
